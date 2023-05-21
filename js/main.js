a34413de75da45108d7c2a361579aa028fb52cae
agustin <33077305+agusc01@users.noreply.github.com>
2023-05-21 04:50:06 -0300

    overflow:hidden

--- js/main.js
+++ js/main.js
@@ -1,40 +1,34 @@
-let magicElement = document.getElementById("matrix");
-let magicContext = magicElement.getContext("2d");
+const magicElement = document.getElementById("matrix");
+const magicContext = magicElement.getContext("2d");
 let width;
 let height;
 let position;
-let velocity;
+const velocity=50;
 
 position = Array(300).join(0).split("");
 
-function resizeScreem()
-{
-  width = document.body.clientWidth;
-  height = document.body.clientHeight;
+function resizeScreem(){
+  width = window.innerWidth;
+  height = window.innerHeight;
   magicElement.width = width;
   magicElement.height = height;
 }
 
-function initMatrix()
-{
+function initMatrix(){
   magicContext.fillStyle = "rgba(0,20,1,0.5)";
   magicContext.fillRect(0, 0, width, height);
-  magicContext.fillStyle = "rgba(0,255,0,1";
+  magicContext.fillStyle = "rgba(0,255,0,1)";
 
   magicContext.font = "1.5rem Arial";
 
-  position.map(function (y, index)
-  {
+  position.map(function (y, index){
     let texto = String.fromCharCode(1e2 + Math.random() * 30);
     let x = index * 15 + 15;
 
     matrix.getContext("2d").fillText(texto, x, y);
-    if (y > (100 + Math.random() * 1e5) )
-    {
+    if (y > (100 + Math.random() * 1e5) ){
       position[index] = 0;
-    } 
-    else
-    {
+    } else {
       position[index] = y +15;
     }
   });
@@ -45,5 +39,4 @@ resizeScreem();
 //to resposive devices
 window.addEventListener("resize",()=>{resizeScreem();});
 
-velocity=50;
 setInterval(initMatrix, velocity);
